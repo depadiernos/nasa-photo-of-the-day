7c57c9950c1d38f7abab2edc2b3f6aa63e385c66
Dennis Padiernos <depadiernos@gmail.com>
2019-08-27 23:04:06 -0500

    use dayjs to allow correct date format for API params.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import Header from "./Components/Header"
 import SidebarList from "./Components/SidebarList"
 import MainImageContainer from "./Components/MainImageContainer"
+import dayjs from 'dayjs'
 import API from "./Utils/getNasaPics"
 
 const Context = createContext()
 
 function App() {
-  const [pictureList, setPictureList] = useState([])
+  const [pictureList, setPictureList] = useState()
+  const [date] = useState(dayjs())
 
+  useEffect(()=>{
+    let isCurrent=true
+    API.get(`neo/rest/v1/feed/?api_key=DEMO_KEY&end_date=${date.format('YYYY-MM-DD')}&start_date=${date.subtract(7, 'day').format('YYYY-MM-DD')}`)
+    .then((res) => {
+      setPictureList(res.data.near_earth_objects)
+    })
+    .catch((err)=> console.log(err))
 
+    return ()=> {isCurrent=false}
+  }, [date])
+
+  console.log(pictureList)
 
   return (
     <div className="App">
       <Context.Provider value={{pictureList, setPictureList}}>
-      <Header />
+      {/* <Header />
       <SidebarList/>
-      <MainImageContainer />
+      <MainImageContainer /> */}
       </Context.Provider>
     </div>
   );
